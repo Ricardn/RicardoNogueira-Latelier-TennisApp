c7306a4051cc34e052ad28d9ec0749ac3e8f7886
Ricardo Nogueira <ricardonogueirafr@outlook.fr>
2022-04-28 20:37:13 +0200

    Create small function to translate Country Code into Country Name

---
+++ front-end/src/utils/countryCodeToName.js
@@ -0,0 +1,20 @@
+let countryCodeToName = [
+  {
+    name: "serbia",
+    code: "SRB",
+  },
+  {
+    name: "united states",
+    code: "USA",
+  },
+  {
+    name: "switzerland",
+    code: "SUI",
+  },
+  {
+    name: "spain",
+    code: "ESP",
+  },
+];
+
+export default countryCodeToName;
