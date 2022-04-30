7770647ac6bc788a079bb99b8ee2936586d15763
Ricardo Nogueira <ricardonogueirafr@outlook.fr>
2022-04-30 18:34:45 +0200

    Create a Loading Skeleton displayed before Data

--- front-end/src/components/skeleton/Skeleton.jsx
+++ front-end/src/components/skeleton/Skeleton.jsx
@@ -2,42 +2,53 @@ import React from "react";
 
 const Skeleton = () => {
   return (
-    <div className="loader_skeleton">
-      <div className="loader_skeletonSearch shine"></div>
-      <div className="loader_skeletonCard shine">
-        <div className="loader_skeletonCardImage"> </div>
-        <div className="loader_skeletonCardColomn">
-          <div className="loader_skeletonCardRow1 s1"> </div>
-          <div className="loader_skeletonCardRow1 s2"> </div>
-          <div className="loader_skeletonCardRow1 s3"> </div>
-          <div className="loader_skeletonCardRow1 s2"> </div>
+    <div className="loader_skeletonBody">
+      <div className="loader_skeleton">
+        <div className="loader_skeletonSearch shine"></div>
+        <div className="loader_skeletonCard shine">
+          <div className="loader_skeletonCardImage"> </div>
+          <div className="loader_skeletonCardColomn">
+            <div className="loader_skeletonCardRow1 s1"> </div>
+            <div className="loader_skeletonCardRow1 s2"> </div>
+            <div className="loader_skeletonCardRow1 s3"> </div>
+            <div className="loader_skeletonCardRow1 s2"> </div>
+          </div>
         </div>
-      </div>
-      <div className="loader_skeletonCard shine">
-        <div className="loader_skeletonCardImage"> </div>
-        <div className="loader_skeletonCardColomn">
-          <div className="loader_skeletonCardRow1 s1"> </div>
-          <div className="loader_skeletonCardRow1 s2"> </div>
-          <div className="loader_skeletonCardRow1 s3"> </div>
-          <div className="loader_skeletonCardRow1 s2"> </div>
+        <div className="loader_skeletonCard shine">
+          <div className="loader_skeletonCardImage"> </div>
+          <div className="loader_skeletonCardColomn">
+            <div className="loader_skeletonCardRow1 s1"> </div>
+            <div className="loader_skeletonCardRow1 s2"> </div>
+            <div className="loader_skeletonCardRow1 s3"> </div>
+            <div className="loader_skeletonCardRow1 s2"> </div>
+          </div>
         </div>
-      </div>
-      <div className="loader_skeletonCard shine">
-        <div className="loader_skeletonCardImage"> </div>
-        <div className="loader_skeletonCardColomn">
-          <div className="loader_skeletonCardRow1 s1"> </div>
-          <div className="loader_skeletonCardRow1 s2"> </div>
-          <div className="loader_skeletonCardRow1 s3"> </div>
-          <div className="loader_skeletonCardRow1 s2"> </div>
+        <div className="loader_skeletonCard shine">
+          <div className="loader_skeletonCardImage"> </div>
+          <div className="loader_skeletonCardColomn">
+            <div className="loader_skeletonCardRow1 s1"> </div>
+            <div className="loader_skeletonCardRow1 s2"> </div>
+            <div className="loader_skeletonCardRow1 s3"> </div>
+            <div className="loader_skeletonCardRow1 s2"> </div>
+          </div>
         </div>
-      </div>
-      <div className="loader_skeletonCard shine">
-        <div className="loader_skeletonCardImage"> </div>
-        <div className="loader_skeletonCardColomn">
-          <div className="loader_skeletonCardRow1 s1"> </div>
-          <div className="loader_skeletonCardRow1 s2"> </div>
-          <div className="loader_skeletonCardRow1 s3"> </div>
-          <div className="loader_skeletonCardRow1 s2"> </div>
+        <div className="loader_skeletonCard shine">
+          <div className="loader_skeletonCardImage"> </div>
+          <div className="loader_skeletonCardColomn">
+            <div className="loader_skeletonCardRow1 s1"> </div>
+            <div className="loader_skeletonCardRow1 s2"> </div>
+            <div className="loader_skeletonCardRow1 s3"> </div>
+            <div className="loader_skeletonCardRow1 s2"> </div>
+          </div>
+        </div>
+        <div className="loader_skeletonCard shine">
+          <div className="loader_skeletonCardImage"> </div>
+          <div className="loader_skeletonCardColomn">
+            <div className="loader_skeletonCardRow1 s1"> </div>
+            <div className="loader_skeletonCardRow1 s2"> </div>
+            <div className="loader_skeletonCardRow1 s3"> </div>
+            <div className="loader_skeletonCardRow1 s2"> </div>
+          </div>
         </div>
       </div>
     </div>
